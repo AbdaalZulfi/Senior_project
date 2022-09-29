26441911affab8fc565a5e73c6e88ccd63d65049
AbdaalZulfi <114151339+AbdaalZulfi@users.noreply.github.com>
2022-09-29 11:58:45 +0300

    Added BluetoothPlugin

---
+++ android/app/src/main/assets/public/src_app_tab2_tab2_module_ts.js
@@ -0,0 +1,188 @@
+"use strict";
+(self["webpackChunkapp"] = self["webpackChunkapp"] || []).push([["src_app_tab2_tab2_module_ts"],{
+
+/***/ 3092:
+/*!*********************************************!*\
+  !*** ./src/app/tab2/tab2-routing.module.ts ***!
+  \*********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Tab2PageRoutingModule": () => (/* binding */ Tab2PageRoutingModule)
+/* harmony export */ });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! tslib */ 4929);
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/core */ 2560);
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/router */ 124);
+/* harmony import */ var _tab2_page__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./tab2.page */ 442);
+
+
+
+
+const routes = [
+    {
+        path: '',
+        component: _tab2_page__WEBPACK_IMPORTED_MODULE_0__.Tab2Page,
+    }
+];
+let Tab2PageRoutingModule = class Tab2PageRoutingModule {
+};
+Tab2PageRoutingModule = (0,tslib__WEBPACK_IMPORTED_MODULE_1__.__decorate)([
+    (0,_angular_core__WEBPACK_IMPORTED_MODULE_2__.NgModule)({
+        imports: [_angular_router__WEBPACK_IMPORTED_MODULE_3__.RouterModule.forChild(routes)],
+        exports: [_angular_router__WEBPACK_IMPORTED_MODULE_3__.RouterModule]
+    })
+], Tab2PageRoutingModule);
+
+
+
+/***/ }),
+
+/***/ 4608:
+/*!*************************************!*\
+  !*** ./src/app/tab2/tab2.module.ts ***!
+  \*************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Tab2PageModule": () => (/* binding */ Tab2PageModule)
+/* harmony export */ });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! tslib */ 4929);
+/* harmony import */ var _ionic_angular__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! @ionic/angular */ 3819);
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @angular/core */ 2560);
+/* harmony import */ var _angular_common__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! @angular/common */ 4666);
+/* harmony import */ var _angular_forms__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! @angular/forms */ 2508);
+/* harmony import */ var _tab2_page__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./tab2.page */ 442);
+/* harmony import */ var _explore_container_explore_container_module__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../explore-container/explore-container.module */ 581);
+/* harmony import */ var _tab2_routing_module__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./tab2-routing.module */ 3092);
+
+
+
+
+
+
+
+
+let Tab2PageModule = class Tab2PageModule {
+};
+Tab2PageModule = (0,tslib__WEBPACK_IMPORTED_MODULE_3__.__decorate)([
+    (0,_angular_core__WEBPACK_IMPORTED_MODULE_4__.NgModule)({
+        imports: [
+            _ionic_angular__WEBPACK_IMPORTED_MODULE_5__.IonicModule,
+            _angular_common__WEBPACK_IMPORTED_MODULE_6__.CommonModule,
+            _angular_forms__WEBPACK_IMPORTED_MODULE_7__.FormsModule,
+            _explore_container_explore_container_module__WEBPACK_IMPORTED_MODULE_1__.ExploreContainerComponentModule,
+            _tab2_routing_module__WEBPACK_IMPORTED_MODULE_2__.Tab2PageRoutingModule
+        ],
+        declarations: [_tab2_page__WEBPACK_IMPORTED_MODULE_0__.Tab2Page]
+    })
+], Tab2PageModule);
+
+
+
+/***/ }),
+
+/***/ 442:
+/*!***********************************!*\
+  !*** ./src/app/tab2/tab2.page.ts ***!
+  \***********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Tab2Page": () => (/* binding */ Tab2Page)
+/* harmony export */ });
+/* harmony import */ var C_Users_mohdz_Documents_GitHub_Senior_project_node_modules_babel_runtime_helpers_esm_asyncToGenerator_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./node_modules/@babel/runtime/helpers/esm/asyncToGenerator.js */ 1670);
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! tslib */ 4929);
+/* harmony import */ var _tab2_page_html_ngResource__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./tab2.page.html?ngResource */ 1748);
+/* harmony import */ var _tab2_page_scss_ngResource__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./tab2.page.scss?ngResource */ 1597);
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! @angular/core */ 2560);
+/* harmony import */ var _awesome_cordova_plugins_android_permissions_ngx__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @awesome-cordova-plugins/android-permissions/ngx */ 1183);
+/* harmony import */ var _awesome_cordova_plugins_bluetooth_serial_ngx__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @awesome-cordova-plugins/bluetooth-serial/ngx */ 8156);
+/* harmony import */ var _ionic_angular__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! @ionic/angular */ 3819);
+
+
+
+
+
+
+
+
+let Tab2Page = class Tab2Page {
+  constructor(bluetoothSerial, permissions, alertCtrl) {
+    this.bluetoothSerial = bluetoothSerial;
+    this.permissions = permissions;
+    this.alertCtrl = alertCtrl;
+  }
+
+  pair() {
+    this.bluetoothSerial.isEnabled().then(succ => {
+      this.permissions.checkPermission(this.permissions.PERMISSION.BLUETOOTH_CONNECT).then(res => {
+        this.bluetoothSerial.list().then(success => {
+          this.pairedDevices = success;
+        }), error => {
+          this.showError("No Paired Devices Found");
+        };
+      }, err => {
+        this.permissions.requestPermission(this.permissions.PERMISSION.BLUETOOTH_CONNECT);
+      });
+    }, err => {
+      this.showError("Please Enable Bluetooth and try again");
+    });
+  }
+
+  showError(error) {
+    var _this = this;
+
+    return (0,C_Users_mohdz_Documents_GitHub_Senior_project_node_modules_babel_runtime_helpers_esm_asyncToGenerator_js__WEBPACK_IMPORTED_MODULE_0__["default"])(function* () {
+      let alert = yield _this.alertCtrl.create({
+        message: error,
+        subHeader: 'Error',
+        buttons: ['OK']
+      });
+      yield alert.present();
+    })();
+  }
+
+};
+
+Tab2Page.ctorParameters = () => [{
+  type: _awesome_cordova_plugins_bluetooth_serial_ngx__WEBPACK_IMPORTED_MODULE_4__.BluetoothSerial
+}, {
+  type: _awesome_cordova_plugins_android_permissions_ngx__WEBPACK_IMPORTED_MODULE_3__.AndroidPermissions
+}, {
+  type: _ionic_angular__WEBPACK_IMPORTED_MODULE_5__.AlertController
+}];
+
+Tab2Page = (0,tslib__WEBPACK_IMPORTED_MODULE_6__.__decorate)([(0,_angular_core__WEBPACK_IMPORTED_MODULE_7__.Component)({
+  selector: 'app-tab2',
+  template: _tab2_page_html_ngResource__WEBPACK_IMPORTED_MODULE_1__,
+  styles: [_tab2_page_scss_ngResource__WEBPACK_IMPORTED_MODULE_2__]
+})], Tab2Page);
+
+
+/***/ }),
+
+/***/ 1597:
+/*!************************************************!*\
+  !*** ./src/app/tab2/tab2.page.scss?ngResource ***!
+  \************************************************/
+/***/ ((module) => {
+
+module.exports = "\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJ0YWIyLnBhZ2Uuc2NzcyJ9 */";
+
+/***/ }),
+
+/***/ 1748:
+/*!************************************************!*\
+  !*** ./src/app/tab2/tab2.page.html?ngResource ***!
+  \************************************************/
+/***/ ((module) => {
+
+module.exports = "<ion-header [translucent]=\"true\">\n  <ion-toolbar>\n    <ion-title>\n      Tab 2\n    </ion-title>\n  </ion-toolbar>\n</ion-header>\n\n<ion-content [fullscreen]=\"true\">\n  <ion-card *ngFor=\"let x of pairedDevices\">\n    <ion-label>{{x.name}}</ion-label><br>\n    <ion-label>{{x.id}}</ion-label><br>\n    <ion-label>{{x.address}}</ion-label><br>\n  </ion-card>\n<ion-button (click)=\"pair()\">Pair Car</ion-button>\n</ion-content>\n";
+
+/***/ })
+
+}]);
+//# sourceMappingURL=src_app_tab2_tab2_module_ts.js.map
